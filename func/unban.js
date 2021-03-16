ab3931e9c8f5ac4d1b1eaf774ddec8ff8e336968
OviiiOne <oviedo.97@hotmail.com>
2021-03-16 12:29:41 +0100

    Traducción al español

--- func/unban.js
+++ func/unban.js
@@ -17,14 +17,14 @@ exports.handler = async function (event, context) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": `/success?msg=${encodeURIComponent("User has been unbanned\nPlease contact them and let them know")}`
+                        "Location": `/success?msg=${encodeURIComponent("El usuario ha sido desbaneado")}`
                     }
                 };
             } catch (e) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": `/error?msg=${encodeURIComponent("Failed to unban user\nPlease manually unban")}`
+                        "Location": `/error?msg=${encodeURIComponent("Fallo al desbanear al usuario\nQuita el ban de forma manual")}`
                     }
                 };
             }
